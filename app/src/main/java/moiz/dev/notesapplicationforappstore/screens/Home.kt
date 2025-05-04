704fc766048309a0f227fac05013b8260e6a5f45
Abdul-Moiz-Khan1 <moizz1326@gmail.com>
2025-05-04 19:55:45 +0500

    add new note screen and added navigation

--- app/src/main/java/moiz/dev/notesapplicationforappstore/screens/Home.kt
+++ app/src/main/java/moiz/dev/notesapplicationforappstore/screens/Home.kt
@@ -1,22 +1,60 @@
 package moiz.dev.notesapplicationforappstore.screens
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import moiz.dev.notesappplaystore.utils.Routes
 
 @Composable
 fun Home(navController: NavController) {
+    var search by remember { mutableStateOf("") }
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 32.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)
     ) {
-        Text("This is home Screen" , fontSize = 36.sp)
+        OutlinedTextField(
+            value = search,
+            onValueChange = { search = it }, label = { Text("Search") },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Search",
+                    tint = Color.Gray
+                )
+            }, modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+
     }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(bottom = 24.dp, end = 24.dp)
+    ) {
+        FloatingActionButton(
+            onClick = { navController.navigate(Routes.NEW) },
+            modifier = Modifier.align(Alignment.BottomEnd)
+        ) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
+        }
+    }
+
 }
--- app/src/main/java/moiz/dev/notesapplicationforappstore/screens/NewNote.kt
+++ app/src/main/java/moiz/dev/notesapplicationforappstore/screens/NewNote.kt
@@ -1,9 +1,34 @@
 package moiz.dev.notesapplicationforappstore.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import moiz.dev.notesapplicationforappstore.R
+import moiz.dev.notesappplaystore.utils.Routes
 
 @Composable
-fun NewNote(navController: NavController,) {
+fun NewNote(navController: NavController) {
+    Image(
+        painter = painterResource(R.drawable.back),
+        null,
+        modifier = Modifier
+            .padding(start = 16.dp, top = 24.dp)
+            .clickable { navController.popBackStack() })
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) { Text("New Note", fontSize = 36.sp) }
 }
